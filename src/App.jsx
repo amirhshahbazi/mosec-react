de8e3ea699848a2f3e19b084e541b174d1e136cc
= <rballshahbazi2@gmail.com>
2022-01-21 12:47:22 +0330

    add sass and define a structure for the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SwitchButton from "./components/SwitchButton";
+
+const App = () => {
+    return (
+        <div>
+            <h1>App</h1>
+            <SwitchButton title={'Switch'}/>
+        </div>
+    )
+}
+
+export default App
