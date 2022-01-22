673b8f1535c6ed0e515afa9729771d101fec5e5d
AmirHosein Shahbazi <Shahbazi@nitaco.local>
2022-01-22 12:12:03 +0330

    convert Container into a class based component

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,34 +1,74 @@
-import React, {useState, useEffect} from "react"
+import React, {Component} from "react"
 import SwitchButton from "../SwitchButton"
+import issuesApi from "../../service/issuesApi"
 import './style.scss'
 
-const Container = (props) => {
-    const [states, setStates] = useState([])
+class Container extends Component {
+    state = {
+        states: [],
+        page: 1,
+        sort: 'created',
+    }
 
+    constructor(props) {
+        super(props)
+    }
 
-    useEffect(() => {
-        // states are here! do something with them!
-        console.log(states)
-    })
+    componentDidMount() {
+        this.getIssues()
+    }
 
-    const showState = (val, buttonName) => {
-        if (!states.includes(buttonName)) {
-            setStates([...states, buttonName])
-        } else if (states.includes(buttonName)) {
-            setStates(states.filter(state => state !== buttonName))
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.getIssues()
+    }
+
+    getIssues = async () => {
+        const state = this.determineState()
+        const {page, sort} = this.state
+        const results = await issuesApi.fetchIssues({
+            state,
+            page,
+            sort,
+        })
+    }
+
+    determineState = () => {
+        const {states} = this.state
+        let state
+
+        if (states.length === 0 || states.length === 2) {
+            state = 'all'
+        } else {
+            state = states[0]
+        }
+
+        return state
+    }
+
+    showState = (val, buttonName) => {
+        if (!this.state.states.includes(buttonName)) {
+            this.setState({
+                states: [...this.state.states, buttonName]
+            })
+        } else if (this.state.states.includes(buttonName)) {
+            this.setState({
+                states: this.state.states.filter(state => state !== buttonName)
+            })
         }
     }
 
-    return (
-        <div className="container">
-            <header className={`container__header`}>
-                <SwitchButton stateChanged={showState} title={`open`} />
-                <SwitchButton stateChanged={showState} title={`closed`} />
-            </header>
-            <main className={`container__body`}>
-            </main>
-        </div>
-    )
+    render() {
+        return (
+            <div className="container">
+                <header className={`container__header`}>
+                    <SwitchButton stateChanged={this.showState} title={`open`}/>
+                    <SwitchButton stateChanged={this.showState} title={`closed`}/>
+                </header>
+                <main className={`container__body`}>
+                </main>
+            </div>
+        )
+    }
 }
 
 export default Container
